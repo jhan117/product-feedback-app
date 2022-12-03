4b98d6553153cb3764f56c5d8184bff11c4b664d
2001Kaye <jhan117@naver.com>
2022-12-03 11:48:53 +0900

    fix: router 위치 변경

--- src/index.js
+++ src/index.js
@@ -16,9 +16,7 @@ root.render(
       <TagsContextProvider>
         <SortContextProvider>
           <StatusContextProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
+            <App />
           </StatusContextProvider>
         </SortContextProvider>
       </TagsContextProvider>
